1424f701f4aa73440187b62a15c72f0902882ffd
Aditya <itsadityagupta0909@gmail.com>
2024-02-13 11:36:14 +0530

    add chat bgImage

--- lib/screens/chat_room.dart
+++ lib/screens/chat_room.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class ChatRoom extends StatelessWidget {
   final String pic;
   final String name;
   final String status;
 
-  const ChatRoom({super.key, required this.pic, required this.name, required this.status});
+  const ChatRoom({
+    super.key,
+    required this.pic,
+    required this.name,
+    required this.status,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
+        backgroundColor: Colors.grey[400],
         appBar: AppBar(
           toolbarHeight: 67,
           leadingWidth: 92,
@@ -20,7 +24,7 @@ class ChatRoom extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Navigator.of(context).pop();
                   },
                   child: Icon(
@@ -45,12 +49,12 @@ class ChatRoom extends StatelessWidget {
             children: [
               Text(name),
               Text(
-               status,
+                status,
                 style: TextStyle(fontSize: 13),
               )
             ],
           ),
-          actions: [
+          actions: const [
             SizedBox(
               width: 15,
             ),
@@ -69,42 +73,65 @@ class ChatRoom extends StatelessWidget {
           ],
         ),
         body: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(child: Container()),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/bg-image.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: const [
+                      // Existing chat messages/widgets
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Row(
               children: [
                 Container(
                   margin: EdgeInsets.all(7),
                   padding: EdgeInsets.all(4),
-                  width: MediaQuery.of(context).size.width*0.80,
-                  height: MediaQuery.of(context).size.height*0.07,
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  height: MediaQuery.of(context).size.height * 0.07,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: Colors.white
-                  ),
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.white),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Icon(Icons.emoji_emotions_outlined, size: 30, color: Colors.grey,),
+                      Icon(
+                        Icons.emoji_emotions_outlined,
+                        size: 30,
+                        color: Colors.grey,
+                      ),
                       Container(
                         padding: EdgeInsets.only(top: 17),
                         // color: Colors.grey,
-                         margin: EdgeInsets.only(top: 3,),
-                        width: MediaQuery.of(context).size.width*0.30,
-                        height: MediaQuery.of(context).size.height*0.10,
+                        margin: EdgeInsets.only(
+                          top: 3,
+                        ),
+                        width: MediaQuery.of(context).size.width * 0.30,
+                        height: MediaQuery.of(context).size.height * 0.10,
                         child: TextField(
-
                           style: TextStyle(fontSize: 20),
                           decoration: InputDecoration(
-                            hintText: 'Message' ,
-                            hintStyle: TextStyle(color: Colors.black45, fontSize: 20),
-                                border: InputBorder.none,
-
+                            hintText: 'Message',
+                            hintStyle:
+                                TextStyle(color: Colors.black45, fontSize: 20),
+                            border: InputBorder.none,
                           ),
                         ),
                       ),
-                      Icon(CupertinoIcons.paperclip, color: Colors.grey[700],),
+                      Icon(
+                        CupertinoIcons.paperclip,
+                        color: Colors.grey[700],
+                      ),
                       Container(
                         width: 25,
                         height: 25,
@@ -112,10 +139,16 @@ class ChatRoom extends StatelessWidget {
                           shape: BoxShape.circle,
                           color: Colors.grey[700],
                         ),
-                        child: Icon(Icons.currency_rupee_outlined, color: Colors.white,size: 17,),
+                        child: Icon(
+                          Icons.currency_rupee_outlined,
+                          color: Colors.white,
+                          size: 17,
+                        ),
                       ),
-                      Icon(Icons.camera_alt,color: Colors.grey[700],),
-
+                      Icon(
+                        Icons.camera_alt,
+                        color: Colors.grey[700],
+                      ),
                     ],
                   ),
                 ),
@@ -126,7 +159,11 @@ class ChatRoom extends StatelessWidget {
                     shape: BoxShape.circle,
                     color: Colors.teal,
                   ),
-                  child: Icon(Icons.mic, size: 30, color: Colors.white,),
+                  child: Icon(
+                    Icons.mic,
+                    size: 30,
+                    color: Colors.white,
+                  ),
                 )
               ],
             )
